e5625b19968e6c354207c0e2ca383bacef4d6a59
reantepattersondev <reantepattersondev@outlook.com>
2017-09-12 19:38:53 -0800

    init commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+const io = require("socket.io")(server);
+const { ExpressPeerServer  } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+});
+app.use("/peerjs", peerServer);
+
+app.set('view engine','ejs');
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+    res.redirect(`/${uuidv4()}`);
+});
+app.get("/:room", (req, res) => {
+    res.render('room',{ roomId: req.param.room });
+});
+io.on("connection", (socket) => {
+    socket.on("join-room", (roomId, userId) => {
+        console.log(roomId)
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit("user-connected", userId);
+    });
+});
+server.listen(3030);
